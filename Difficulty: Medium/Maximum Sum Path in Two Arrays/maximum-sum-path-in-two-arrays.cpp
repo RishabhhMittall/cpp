bb1ea5c5ab8c837d910f99ba28bb0b1bba5fbdf3
Rishabh Mittal <155389183+RishabhhMittall@users.noreply.github.com>
2025-08-10 17:19:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Sum Path in Two Arrays/maximum-sum-path-in-two-arrays.cpp
@@ -0,0 +1,48 @@
+// User function template for C++
+class Solution {
+  public:
+    int maxPathSum(vector<int>& arr1, vector<int>& arr2) {
+        int ans = 0;
+        
+        int n = arr1.size();
+        int m = arr2.size();
+        
+        sort(arr1.begin(), arr1.end());
+        sort(arr2.begin(), arr2.end());
+        
+        int i = 0,j = 0;
+        int sum1 = 0, sum2 = 0;
+        
+        while(i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                sum1 += arr1[i];
+                i++;
+            }
+            else if (arr1[i] > arr2[j]) {
+                sum2 += arr2[j];
+                j++;
+            }
+            else {
+                ans += max(sum1, sum2) + arr1[i];
+                i++;
+                j++;
+                sum1  = 0;
+                sum2 = 0;
+            }
+        }
+        // cout << "Out of the loop: " << sum1 << "-" << sum2 << endl;
+        
+        while(i < n) {
+            sum1 += arr1[i];
+            i++;
+        }
+        while(j < m) {
+            sum2 += arr2[j];
+            j++;
+        }
+        
+        ans += max(sum1, sum2);
+        
+        return ans;
+    }
+};
